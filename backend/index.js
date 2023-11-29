fc37010883a1b098ee765c1e52a8a4c0faf77efc
sneha0908 <snehasonu123.6@gmail.com>
2023-11-29 12:06:53 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/vcet')
+    .then(() => {
+        console.log('Connected to vcet database');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+const UserSchema = new mongoose.Schema({
+   
+    email: { type: String, required: true, unique: true },
+    
+});
+
+
+const User = mongoose.model('details', UserSchema);
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.post('/register', async (req, resp) => {
+    try {
+        const user = new User(req.body);
+        const result = await user.save();
+        const userWithoutPassword = result.toObject();
+        delete userWithoutPassword.password;
+
+        resp.send(userWithoutPassword);
+        console.log(userWithoutPassword);
+    } catch (e) {
+        console.error(e);
+        resp.status(500).send('Something Went Wrong');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
